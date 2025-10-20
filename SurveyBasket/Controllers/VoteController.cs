c8da8c5a6e42ee6dbcacb6b2c6bd88a276da99cc
YouSry3 <myosre122@gmail.com>
2025-10-20 22:24:13 +0300

    Create 'Start' API EndPoint in VoteController

---
+++ SurveyBasket/Controllers/VoteController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyBasket.Controllers
+{
+    [Route("api/polls/{pollId}/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class VoteController(IQuestionService questionService) : ControllerBase
+    {
+        private readonly IQuestionService QuestionService = questionService;
+
+        [HttpGet]
+        public async Task<IActionResult> Start([FromRoute]int pollId , CancellationToken cancellationToken)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var result = await QuestionService.GetAvaildableAsync(pollId, userId!, cancellationToken);
+
+            return result.IsSuccess?
+                Ok(result.Value)
+                : result.Error!.Equals(VoteErrors.DuplicateVotes())
+                    ? result.ToProblem(StatusCodes.Status406NotAcceptable)
+                    : result.ToProblem(StatusCodes.Status404NotFound);
+
+        }
+    }
+}
